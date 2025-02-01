b8e8de308625df28fe67b26bd88dfa95bc2ef94f
Yotam H <yotam4h@gmail.com>
2025-02-01 15:42:38 +0200

    - idb as life michael would do

--- src/idb/idb.js
+++ src/idb/idb.js
@@ -1,73 +1,76 @@
 /**
  * @file idb.js
- * @class IndexedDBHandler
- * This file provides a class for handling IndexedDB operations.
+ * This file provides a module for handling IndexedDB operations.
  *
- * @exports idb
  * @createdBy Yotam Haimovitch & Raziel Otick
  */
-class IndexedDBHandler {
-    constructor(dbName, version) {
+
+// The module object
+const idb = {};
+
+/**
+ * Open the IndexedDB database.
+ * @param dbName - The name of the database
+ * @param version - The version of the database
+ * @returns {Promise<unknown>}
+ */
+idb.openCostsDB = async function (dbName, version) {
+    return new Promise((resolve, reject) => {
+        // Check for support
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+        // If not supported, reject the promise
+        if (!window.indexedDB) {
+            reject('IndexedDB not supported');
+        }
+
+        // Set the database name and version
         this.dbName = dbName;
         this.version = version;
-        this.db = null;
-    }
 
-    /**
-     * Open the IndexedDB database.
-     * @returns {Promise<unknown>} - The promise object representing the operation
-     */
-    async openCostsDB() {
-        return new Promise((resolve, reject) => {
-            const request = indexedDB.open(this.dbName, this.version);
+        // Open the database
+        const request = indexedDB.open(this.dbName, this.version);
 
-            request.onupgradeneeded = (event) => {
-                const db = event.target.result;
-                if (!db.objectStoreNames.contains("costs")) {
-                    db.createObjectStore("costs", { keyPath: "id", autoIncrement: true });
-                }
-            };
+        // Set the event handlers
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains("costs")) {
+                db.createObjectStore("costs", { keyPath: "id", autoIncrement: true });
+            }
+        };
 
-            request.onsuccess = (event) => {
-                this.db = event.target.result;
-                resolve(this);
-            };
+        request.onsuccess = (event) => {
+            this.db = event.target.result;
+            resolve(this);
+        };
 
-            request.onerror = (event) => {
-                reject(`Error opening IndexedDB: ${event.target.errorCode}`);
-            };
-        });
-    }
-
-    /**
-     * Add a cost to the IndexedDB database.
-     * @param cost - The cost object to add
-     * @returns {Promise<unknown>}
-     */
-    async addCost(cost) {
-        return new Promise((resolve, reject) => {
-            const transaction = this.db.transaction("costs", "readwrite");
-            const store = transaction.objectStore("costs");
-            const request = store.add(cost);
-
-            request.onsuccess = () => {
-                resolve(true);
-            };
-
-            request.onerror = () => {
-                reject(false);
-            };
-        });
-    }
+        request.onerror = (event) => {
+            reject(`Error opening IndexedDB: ${event.target.errorCode}`);
+        };
+    });
 }
 
 /**
- * IndexedDB module.
- * @type {{openCostsDB: (function(dbName, version): Promise<unknown>)}}
+ * Add a cost to the IndexedDB database.
+ * @param cost - The cost object to add
+ * @returns {Promise<unknown>}
  */
-export const idb = {
-    openCostsDB: async (dbName, version) => {
-        const handler = new IndexedDBHandler(dbName, version);
-        return handler.openCostsDB();
-    }
-};
+idb.addCost = async function (cost) {
+    return new Promise((resolve, reject) => {
+        // Create a transaction and add the cost
+        const transaction = this.db.transaction("costs", "readwrite");
+        // Get the object store
+        const store = transaction.objectStore("costs");
+        // Add the cost
+        const request = store.add(cost);
+
+        // Set the event handlers
+        request.onsuccess = () => {
+            resolve(true);
+        };
+
+        request.onerror = () => {
+            reject(false);
+        };
+    });
+}
